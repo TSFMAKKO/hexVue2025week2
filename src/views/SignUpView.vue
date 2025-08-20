2cc576c92d8b2275ae64fc6b72bf1c4ce726098b
yuchin <racingf1@gmail.com>
2025-08-20 20:56:19 +0800

    feat: 更新註冊頁面

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -4,54 +4,34 @@
     <div class="conatiner signUpPage vhContainer">
       <div class="side">
         <a href="#">
-          <img class="logoImg" 
-               src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png" 
-               alt="">
+          <img class="logoImg"
+            src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/logo.png" alt="">
         </a>
-        <img class="d-m-n" 
-             src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png" 
-             alt="workImg">
+        <img class="d-m-n"
+          src="https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/todolist/img.png"
+          alt="workImg">
       </div>
       <div>
-        <form class="formControls" @submit.prevent="handleSignUp">
+        <form class="formControls" @submit.prevent="register">
           <h2 class="formControls_txt">註冊帳號</h2>
-          
+
           <label class="formControls_label" for="email">Email</label>
-          <input class="formControls_input" 
-                 type="email" 
-                 id="email" 
-                 v-model="email" 
-                 placeholder="請輸入 email" 
-                 required>
-          
+          <input class="formControls_input" type="email" id="email" v-model="email" placeholder="請輸入 email" required>
+
           <label class="formControls_label" for="name">您的暱稱</label>
-          <input class="formControls_input" 
-                 type="text" 
-                 id="name" 
-                 v-model="name" 
-                 placeholder="請輸入您的暱稱"
-                 required>
-          
+          <input class="formControls_input" type="text" id="name" v-model="nickname" placeholder="請輸入您的暱稱" required>
+
           <label class="formControls_label" for="pwd">密碼</label>
-          <input class="formControls_input" 
-                 type="password" 
-                 id="pwd" 
-                 v-model="password" 
-                 placeholder="請輸入密碼" 
-                 required>
-          
+          <input class="formControls_input" type="password" id="pwd" v-model="password" placeholder="請輸入密碼" required>
+
           <label class="formControls_label" for="confirmPwd">再次輸入密碼</label>
-          <input class="formControls_input" 
-                 type="password" 
-                 id="confirmPwd" 
-                 v-model="confirmPassword" 
-                 placeholder="請再次輸入密碼" 
-                 required>
-          
+          <input class="formControls_input" type="password" id="confirmPwd" v-model="confirmPassword"
+            placeholder="請再次輸入密碼" required>
+
           <div v-if="error" class="error-message">{{ error }}</div>
-          
+
           <button type="submit" class="formControls_btnSubmit">註冊帳號</button>
-          
+
           <router-link to="/login" class="formControls_btnLink">登入</router-link>
         </form>
       </div>
@@ -64,42 +44,105 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-const email = ref('');
-const name = ref('');
-const password = ref('');
-const confirmPassword = ref('');
+const email = ref('example444@gmail.com');
+// const name = ref('');
+const password = ref('12345678');
+const confirmPassword = ref('12345678');
+const nickname = ref('example444');
 const error = ref('');
+const isLoading = ref(false);
+const hasAccount = ref(false);
+const baseApiUrl = "https://todolist-api.hexschool.io";
+
 
 const handleSignUp = () => {
   // 重置錯誤訊息
   error.value = '';
-  
+
   // 驗證表單
   if (!email.value || !name.value || !password.value || !confirmPassword.value) {
     error.value = '請填寫所有欄位';
     return;
   }
-  
+
   if (password.value !== confirmPassword.value) {
     error.value = '兩次輸入的密碼不一致';
     return;
   }
-  
+
   if (password.value.length < 6) {
     error.value = '密碼長度至少需要6個字元';
     return;
   }
-  
+
   // 這裡應該發送 API 請求進行註冊
   // 模擬註冊成功
-  console.log('註冊資訊:', { 
-    email: email.value, 
-    name: name.value, 
-    password: password.value 
+  console.log('註冊資訊:', {
+    email: email.value,
+    name: name.value,
+    password: password.value
   });
-  
+
   // 註冊成功後跳轉到登入頁面
   router.push('/login');
+};
+
+const register = () => {
+  // 重置錯誤訊息
+  error.value = '';
+
+  // 驗證表單
+  if (!email.value || !nickname.value || !password.value || !confirmPassword.value) {
+    error.value = '請填寫所有欄位';
+    return;
+  }
+  if (password.value !== confirmPassword.value) {
+    error.value = '兩次輸入的密碼不一致';
+    return;
+  }
+
+  if (password.value.length < 6) {
+    error.value = '密碼長度至少需要6個字元';
+    return;
+  }
+
+
+  // 這裡應該發送 API 請求進行註冊
+  console.log("register");
+  isLoading.value = true;
+  //
+  fetch(baseApiUrl + "/users/sign_up", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+      nickname: nickname.value,
+    }),
+  })
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // alert("註冊成功")
+      if (data.status) {
+        alert(`註冊成功 ${data.uid}`);
+        hasAccount.value = true;
+        // 註冊成功後跳轉到登入頁面
+        router.push('/login');
+      } else {
+        alert(`註冊失敗${data.message}`);
+      }
+      isLoading.value = false;
+    })
+    .catch((e) => {
+      alert(`註冊失敗`);
+      isLoading.value = false;
+    });
 };
 </script>
 
